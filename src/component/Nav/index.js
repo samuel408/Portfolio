2a984b8280f77f4e6a83556659f10bb0cc74ff57
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-05-04 21:15:40 -0700

    added media query

--- src/component/Nav/index.js
+++ src/component/Nav/index.js
@@ -10,26 +10,26 @@ function Nav(props){
       } = props;
     return(
         
-        <header className="" >
+        <header className="navbar " >
 
 
   <h1 className=" name col-sm">
       Samuel Saldivar
   </h1>   
-   <nav>
+   <nav className="navbar-collapse " id="navbarSupportedContent">
    <div className=" container">
-  <div className=" marginTop row">
+  <div className=" mediaQ marginTop row ">
  
-    <a  className=" firstNav col-sm" href="#About" onClick={() => setPageSelected("about")}>
+    <a  className=" navbar-nav mr-auto firstNav col-sm" href="#About" onClick={() => setPageSelected("about")}>
       About Me
     </a>
-    <a href="#Work"  onClick={() => setPageSelected("portfolio")} className=" contact col-sm">      Portfolio
+    <a href="#Work"  onClick={() => setPageSelected("portfolio")} className="navbar-nav mr-auto contact col-sm">      Portfolio
     </a>
-    <a href="#Contact"  onClick={() => setPageSelected("contact")} className=" contact col-sm">
+    <a href="#Contact"  onClick={() => setPageSelected("contact")} className="navbar-nav mr-auto contact col-sm">
       Contact
     </a>
 
-    <a href="#Resume"  onClick={() => setPageSelected("resume")} className=" contact col-sm">
+    <a href="#Resume"  onClick={() => setPageSelected("resume")} className=" bottommm navbar-nav mr-auto contact col-sm">
               Resume
     </a>
   </div>
